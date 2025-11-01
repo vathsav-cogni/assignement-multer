9d6ae5258d298790a70a8a7ccaf28728339b676c
vathsav-cogni <Bukka.SaiSriVathsav@cognizant.com>
2025-11-01 17:20:51 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,10 @@
+require('dotenv').config();
+const app = require('./app');
+
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
 
